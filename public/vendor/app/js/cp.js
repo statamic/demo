1bed51022975ce224717635624264a34f7a543cd
Jack McDade <jack@jackmcdade.com>
2022-11-11 10:32:40 -0500

    More resposive

--- public/vendor/app/js/cp.js
+++ public/vendor/app/js/cp.js
@@ -1,12 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!****************************!*\
-  !*** ./resources/js/cp.js ***!
-  \****************************/
-// Pre-populate login creds
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.querySelector('.outside #input-email').value = "david.hasselhoff@example.com";
-  document.querySelector('.outside #input-password').value = "secretsecrets";
-});
-/******/ })()
-;
+document.addEventListener("DOMContentLoaded",(function(e){document.querySelector(".outside #input-email").value="david.hasselhoff@example.com",document.querySelector(".outside #input-password").value="secretsecrets"}));
