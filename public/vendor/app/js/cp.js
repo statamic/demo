71863bd46c9f9bb1e2fa12b9772aaa856fff0a75
Jack McDade <jack@jackmcdade.com>
2023-03-27 09:30:39 -0400

    composer update

--- public/vendor/app/js/cp.js
+++ public/vendor/app/js/cp.js
@@ -1,1 +1,12 @@
-document.addEventListener("DOMContentLoaded",(function(e){document.querySelector(".outside #input-email").value="david.hasselhoff@example.com",document.querySelector(".outside #input-password").value="secretsecrets"}));
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************!*\
+  !*** ./resources/js/cp.js ***!
+  \****************************/
+// Pre-populate login creds
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelector('.outside #input-email').value = "david.hasselhoff@example.com";
+  document.querySelector('.outside #input-password').value = "secretsecrets";
+});
+/******/ })()
+;
