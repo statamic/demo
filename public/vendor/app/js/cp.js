603f905404fbd598260e7a00b053260747d6d91f
Jason Varga <jason@pixelfear.com>
2024-11-07 14:50:04 -0500

    Add welcome message.
    Using JS to avoid publishing a view, and we already have some login tweaks.

--- public/vendor/app/js/cp.js
+++ public/vendor/app/js/cp.js
@@ -3,10 +3,31 @@ var __webpack_exports__ = {};
 /*!****************************!*\
   !*** ./resources/js/cp.js ***!
   \****************************/
-// Pre-populate login creds
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 document.addEventListener("DOMContentLoaded", function (event) {
+  var _welcome$classList;
+
+  // Pre-populate login creds
   document.querySelector('.outside #input-email').value = "david.hasselhoff@example.com";
-  document.querySelector('.outside #input-password').value = "secretsecrets";
+  document.querySelector('.outside #input-password').value = "secretsecrets"; // Add welcome message
+
+  var welcome = document.createElement('div');
+
+  (_welcome$classList = welcome.classList).add.apply(_welcome$classList, _toConsumableArray("max-w-sm mx-auto mb-4 text-center text-xs border border-yellow-dark rounded p-4 bg-yellow dark:bg-dark-blue-100 dark:border-none".split(' ')));
+
+  welcome.innerHTML = "\n        <p class=\"mb-4\">\uD83D\uDC4B Welcome to the Statamic Demo.</p>\n        <p class=\"mb-4\">Feel free to log in using the provided account, take a look around, and see how your actions are reflected on the front-end.</p>\n        <p>Every 30 minutes everything will be reverted to a clean slate.</p>\n    ";
+  document.querySelector('.logo').insertAdjacentElement('afterend', welcome);
 });
 /******/ })()
 ;
--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,6 +1,16 @@
-// Pre-populate login creds
 document.addEventListener("DOMContentLoaded", function(event) {
+    // Pre-populate login creds
+    document.querySelector('.outside #input-email').value = "david.hasselhoff@example.com";
+    document.querySelector('.outside #input-password').value = "secretsecrets";
 
-        document.querySelector('.outside #input-email').value = "david.hasselhoff@example.com";
-        document.querySelector('.outside #input-password').value = "secretsecrets";
+    // Add welcome message
+    var welcome = document.createElement('div');
+    welcome.classList.add(...`max-w-sm mx-auto mb-4 text-center text-xs border border-yellow-dark rounded p-4 bg-yellow dark:bg-dark-blue-100 dark:border-none`.split(' '));
+    welcome.innerHTML = `
+        <p class="mb-4">👋 Welcome to the Statamic Demo.</p>
+        <p class="mb-4">Feel free to log in using the provided account, take a look around, and see how your actions are reflected on the front-end.</p>
+        <p>Every 30 minutes everything will be reverted to a clean slate.</p>
+    `;
+
+    document.querySelector('.logo').insertAdjacentElement('afterend', welcome);
 });
