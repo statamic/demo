6b69263482f95ead801487ecd68fdb618eba389d
Duncan McClean <duncan@duncanmcclean.com>
2026-02-11 11:57:33 +0000

    Fix auth welcome banner on mobile

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -19,12 +19,16 @@ router.on('navigate', (event) => {
 
         // Add welcome message
         var welcome = document.createElement('div');
-        welcome.classList.add(...`absolute top-0 w-full mx-auto text-left text-sm p-4 bg-yellow dark:bg-dark-blue-100 dark:border-none shadow-lg`.split(' '));
+        welcome.classList.add(...`w-full mx-auto text-left text-sm p-4 bg-yellow dark:bg-dark-blue-100 dark:border-none shadow-lg`.split(' ')); // These classes are included in Statamic's CSS
+        welcome.style.marginTop = '-0.5rem'; // These don't have equivalent in Statamic's CSS
+        welcome.style.marginLeft = '-0.5rem';
+        welcome.style.marginRight = '-0.5rem';
+        welcome.style.width = 'calc(100% + 1rem)';
         welcome.innerHTML = `
         <p>👋 <strong>Welcome to the Statamic Demo.</strong> Log in using the provided account, take a look around, and see how your actions are reflected on the front-end. Every 30 minutes all changes are wiped clean, so don't worry too much about breaking anything.</p>
     `;
 
-        document.querySelector('body').insertAdjacentElement('afterend', welcome);
+        document.querySelector('body').insertAdjacentElement('afterbegin', welcome);
         document.querySelector('.logo').style.marginTop = '30px';
     }
 });
